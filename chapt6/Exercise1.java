636ff2639655217ce274a923be0c49ecb29f9de0
Brock Leatherman <bleatherman@java.nas.local>
2013-11-13 10:15:32 -0500

    Added new file to repository

---
+++ chapt6/Exercise1.java
@@ -0,0 +1,22 @@
+class Exercise1 {
+
+  public static void main(String[] args)
+  {
+   int n;
+    n = 2;
+   int m;
+    m = 4;
+	boolean flag;
+	 flag = true;
+	boolean testResult = false;
+	boolean evenFlag = (n/m == 0);
+	boolean positiveFlag = (n > 0);
+    if (evenFlag) {
+      System.out.println("true");
+    }else{
+      System.out.println("false");
+  }
+
+ }
+}
+
