02bb4c462c50edbe3cc33c491eab8c9b36a0f657
Brock Leatherman <bleatherman@java.nas.local>
2013-11-21 10:02:03 -0500

    Added new respoistory

---
+++ chapt6/Exercise5.java
@@ -0,0 +1,9 @@
+class Exercise5 {
+  public static void main(String[] args){
+  System.out.println(sumSquare(1.0, 2.0));
+ }
+  public static double sumSquare(double x, double y){
+    return ((x*x) + (y*y));
+  }
+}
+
